d9d351e5c5b71714495d12af4ca0324525b21478
Jooney Yeon <55115412+yeonseongjun@users.noreply.github.com>
2022-12-02 15:35:35 -0800

    changed to letters

--- 2022/day02/part_2.py
+++ 2022/day02/part_2.py
@@ -2,14 +2,13 @@ score = 0
 my_dict = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
 data = open("input.txt").read()
 for line in data.split("\n"):
-    opponent, result = my_dict[line[0]], my_dict[line[-1]]
-    if result == 1:
+    opponent, result = my_dict[line[0]], line[-1]
+    if result == 'X':
         my_choice = opponent - 1 if opponent - 1 > 0 else 3
         score += my_choice
-    if result == 2:
+    if result == 'Y':
         score += 3 + opponent
-    if result == 3:
+    if result == 'Z':
         my_choice = opponent + 1 if opponent + 1 < 4 else 1
         score += 6 + my_choice
 print(score)
-        
