05ff30211d566266e6812152a2416a92dea39d30
Jooney Yeon <55115412+yeonseongjun@users.noreply.github.com>
2022-12-02 15:21:31 -0800

    better naming

--- 2022/day02/part_2.py
+++ 2022/day02/part_2.py
@@ -4,15 +4,12 @@ data = open("input.txt").read()
 for line in data.split("\n"):
     opponent, result = my_dict[line[0]], my_dict[line[-1]]
     if result == 1:
-        temp = opponent - 1 if opponent - 1 > 0 else 3
-        score += temp
-
+        my_choice = opponent - 1 if opponent - 1 > 0 else 3
+        score += my_choice
     if result == 2:
         score += 3 + opponent
-
     if result == 3:
-        temp = opponent + 1 if opponent + 1 < 4 else 1
-        score += 6 + temp
-
+        my_choice = opponent + 1 if opponent + 1 < 4 else 1
+        score += 6 + my_choice
 print(score)
         
